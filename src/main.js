d2aaad768acd857b9191170b1e193879844e0801
codwerk <louis_widdy@live.fr>
2020-06-24 17:14:32 +0200

    import bootstrap-vue, bootstrap (scss)

--- src/main.js
+++ src/main.js
@@ -1,7 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue } from 'bootstrap-vue'
 
 Vue.config.productionTip = false
+
+// Install BootstrapVue
+Vue.use(BootstrapVue)
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 
 new Vue({
   render: h => h(App),
