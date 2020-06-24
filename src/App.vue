981ecb92a0f42883cdfceceba17dd32bdc1ff1c8
codwerk <louis_widdy@live.fr>
2020-06-24 17:09:22 +0200

    import component slider

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Slider></Slider>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Slider from './components/includes/Slider'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Slider
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+
+@import url('./assets/scss/index.scss');
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+div div button {
+  outline: none;
 }
 </style>
